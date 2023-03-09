4b3fe30481b966b8b57cf8a123971477a4cf5f94
Kong0204 <andrinakongkai@gmail.com>
2023-03-09 13:59:15 +0800

    how to use Binding instead of findViewById

--- app/src/main/java/com/example/practicalw4g124/MainActivity.kt
+++ app/src/main/java/com/example/practicalw4g124/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.practicalw4g124
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.practicalw4g124.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //put everything into this binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //so now i no longer need this code
+//        findViewById<Button>(R.id.done_button).setOnClickListener{
+//            addNickName(it)
+//        }
+        binding.doneButton.setOnClickListener{
+            addNickName(it)
+        }
+
+        //so now i no longer need this code
+//        findViewById<TextView>(R.id.nickname_text).setOnClickListener{
+//            updateNickname(it)
+//        }
+        binding.nicknameText.setOnClickListener{
+            updateNickName(it)
+        }
     }
+
+
+    private fun addNickName(view: View){
+        //after use binding, i no longer need this code
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView = findViewById<TextView>(R.id.nickname_edit)
+
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+        val imm=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+
+    }
+
+    private fun updateNickName(view:View){
+
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val doneButton = findViewById<Button>(R.id.done_button)
+
+        binding.nicknameEdit.visibility = View.VISIBLE
+        binding.doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        binding.nicknameEdit.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.nicknameEdit,0)
+
+    }
+
+
+
+
 }
